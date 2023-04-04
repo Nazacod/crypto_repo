3e49051a3c67de5610474af008ea419f71bde417
Danila Kovtun <danila.kovtunn@gmail.com>
2023-04-04 13:58:19 +0300

    sha3-256 done

---
+++ sha3-256/sha3-test.c
@@ -0,0 +1,72 @@
+#include "sha3.h"
+#include "util.h"
+
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define OPTIONS "hvl:i:o:"
+
+void print_usage(char **argv) {
+    fprintf(stderr,
+        "SYNOPSIS\n"
+        "   Hashes inputs using the SHA-3 256 algorithm.\n"
+        "\n"
+        "USAGE\n"
+        "   %s [-hv] -l length [-i input] [-o output]\n"
+        "\n"
+        "OPTIONS\n"
+        "   -h          Display program help and usage\n"
+        "   -v          Display verbose program output\n"
+        "   -l length   Length of input in bytes\n"
+        "   -i input    Specify input to hash (stdin by default)\n",
+        argv[0]);
+}
+
+void hash_file(int64_t length, FILE *infile) {
+    uint8_t *msg = (uint8_t *)calloc(length ? length : 1, sizeof(uint8_t));
+    check(msg, "Failed to allocate array for message.\n");
+
+    fread(msg, sizeof(uint8_t), length, infile);
+
+    uint8_t md[SHA3_256_MD_LEN] = { 0 };
+    sha3_256_digest(msg, length, md);
+
+    hexprint(SHA3_256_MD_LEN, md);
+
+    free(msg);
+}
+
+int main(int argc, char **argv) {
+    int opt = 0;
+    int64_t length = -1;
+    FILE *infile = stdin;
+
+    verbose = true;
+    
+    while ((opt = getopt(argc, argv, OPTIONS)) != -1) {
+        switch (opt) {
+        case 'h':
+            print_usage(argv);
+            return 0;
+        case 'l':
+            length = (int64_t)strtoll(optarg, NULL, 10);
+            break;
+        case 'i':
+            infile = fopen(optarg, "rb");
+            check(infile, "Failed to open %s.\n", optarg);
+            break;
+        default:
+            print_usage(argv);
+            return 1;
+        }
+    }
+
+    check(length >= 0, "Valid message length must be supplied.\n");
+
+    hash_file(length, infile);
+
+    fclose(infile);
+    return 0;
+}
