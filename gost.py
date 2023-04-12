82c3e44f6bf19b96c615dfee526a07d09cff53fc
Захар Назаров <s02190158@live.cs.msu.ru>
2023-04-12 22:21:29 +0300

    delete comment

--- gost.py
+++ gost.py
@@ -1,7 +1,5 @@
-import os
-import hashlib
 import random
-from tinyec import registry, ec
+from tinyec import registry
 from gostcrypto import gosthash
 
 def gost_hash(data_):
@@ -47,23 +45,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-# def main():
-#     p = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFD97
-#     a = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFD94
-#     b = 0x5db5e5d5ca3279b6edc8e1d24d275e7534e4f1a4c6a96e6e4d9ed7ee8c0a47de
-#     gx = 0x2
-#     gy = 0x5db5e5d5ca3279b6edc8e1d24d275e7534e4f1a4c6a96e6e4d9ed7ee8c0a47de
-#     n = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF6C611070995AD10045841B09B761B893
-#     curve = ec.Curve(a, b, p, n, gx, gy, "GostR3410_2012_TC26_ParamSetA")
-
-#     data = b'Test message for digital signature'
-#     private_key, public_key = generate_key_pair(curve)
-#     signature = sign_data(curve, private_key, data)
-#     print(f'Signature: {signature}')
-#     is_valid = verify_signature(curve, public_key, data, signature)
-#     print(f'Is signature valid? {is_valid}')
-
-# if __name__ == '__main__':
-#     main()
